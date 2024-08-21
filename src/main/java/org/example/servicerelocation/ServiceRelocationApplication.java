22358e4516ef6f21e3be2c283279f21c80e2f054
igr76 <gritsuk766@gmail.com>
2024-08-21 22:15:40 +1000

    start

---
+++ src/main/java/org/example/servicerelocation/ServiceRelocationApplication.java
@@ -0,0 +1,13 @@
+package org.example.servicerelocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRelocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRelocationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/servicerelocation/ServiceRelocationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servicerelocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRelocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
